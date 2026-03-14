f0109e7fc5c9896c77b2c0b66317966d22fd9d0d
Danee Jhajaira Barzola Navarro <1923010381@untels.edu.pe>
2026-03-14 01:17:14 -0500

    chore: setup backend dependencies and modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
+import { AlertsModule } from './alerts/alerts.module';
+import { RealtimeModule } from './realtime/realtime.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [],
+  imports: [EventsModule, AlertsModule, RealtimeModule, RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
